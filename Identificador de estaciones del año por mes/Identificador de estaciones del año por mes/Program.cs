3ff44f5d8f5229d21e7ed2a57ae3c38f2ff940f3
2025-0251-png <2025-0251@unad.edu.do>
2026-06-10 23:23:43 -0400

    Add files via upload

---
+++ Identificador de estaciones del año por mes/Identificador de estaciones del año por mes/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("escriba el mes que necesite");
+string mes = Console.ReadLine();
+
+switch (mes)
+{
+    case "diciembre":
+    case "enero":
+    case "febrero":
+        Console.WriteLine("estamos en invierno");
+        break;
+    case "marzo":
+    case "abril":
+    case "mayo":
+        Console.WriteLine("estamos en primavera");
+        break;
+    case "junio":
+    case "julio":
+    case "agosto":
+        Console.WriteLine("estamos en verano");
+        break;
+    case "septiembre":
+    case "octubre":
+    case "noviembre":
+        Console.WriteLine("estamos en otoño");
+        break;
+    default:
+        Console.WriteLine("mes no valido");
+        break;
+}
---
+++ mayor de edad/mayor de edad/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("INGRESE SU NOMBRE");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("ingrese su edad"); 
+int edad = int.Parse(Console.ReadLine());
+
+if (edad >= 18)
+
+Console.WriteLine("Usted es mayor de edad, biemvenido sr/a " + nombre);
+
+else
+    Console.WriteLine("Usted es menor de edad, lo siento");
+
