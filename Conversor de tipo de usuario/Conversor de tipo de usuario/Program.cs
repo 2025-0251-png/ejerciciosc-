c393652c91475554b1c54b71965f8d4367eae2e8
2025-0251-png <2025-0251@unad.edu.do>
2026-06-10 23:32:27 -0400

    Add files via upload

---
+++ Conversor de tipo de usuario/Conversor de tipo de usuario/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("ingrese letra clave (A,E,U,L)");
+string letra = Console.ReadLine();
+
+switch (letra)
+
+{
+    case "A":
+        Console.WriteLine("usted accede como Administrador tiene acceso a todo");
+        break;
+    case "E":
+        Console.WriteLine("usted accede como Editor puede editar los proyectos");
+        break;
+    case "U":
+        Console.WriteLine("usted accede como Autor puede redactar sus proyectos");
+        break;
+    case "L":
+        Console.WriteLine("usted accede como Lector solo puede leer los proyectos");
+        break;
+    default:
+        Console.WriteLine("usted accede como invitado");
+        break;
+    }
