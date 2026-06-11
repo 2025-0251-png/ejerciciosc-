9b79bff04c933363884541d91a432a126fd3d7a4
2025-0251-png <2025-0251@unad.edu.do>
2026-06-10 23:33:29 -0400

    Add files via upload

---
+++ Una máquina de autoservicio/Una máquina de autoservicio/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("seleccione el tipo de bebida que desea 1: Café, 2: Té, 3: Chocolate, 4: Capuchino");
+string bebida = Console.ReadLine();
+
+switch (bebida)
+{ 
+    case "1":
+        Console.WriteLine("Has seleccionado Café - costo 100$");
+        break;
+    case "2":
+        Console.WriteLine("Has seleccionado Té - costo 60$");
+        break;
+    case "3":
+        Console.WriteLine("Has seleccionado Chocolate - costo 75$ ");
+        break;
+    case "4":
+        Console.WriteLine("Has seleccionado Capuchino - costo 120$ ");
+        break;
+    default:
+        Console.WriteLine("Opción no válida");
+        break;
+}
