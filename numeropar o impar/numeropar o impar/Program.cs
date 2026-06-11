c86cba7329107de12b2256573f41f15df0b51499
2025-0251-png <2025-0251@unad.edu.do>
2026-06-10 23:35:44 -0400

    Add files via upload

---
+++ numeropar o impar/numeropar o impar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("ingrese un numero");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("el numero es par");
+        }
+        else
+        {
+            Console.WriteLine("el numero es impar.");
+        }
+    }
+}
