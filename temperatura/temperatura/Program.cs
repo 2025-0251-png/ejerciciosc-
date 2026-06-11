2d5c8d88bc98d7f6f46d4e12fe0a368e237bfe69
2025-0251-png <2025-0251@unad.edu.do>
2026-06-10 23:41:17 -0400

    Add files via upload

---
+++ temperatura/temperatura/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("ingrese temperatura");
+int temperatura = int.Parse(Console.ReadLine());
+
+if (temperatura <= 15)
+{
+    Console.WriteLine("esta haciendo frio");
+}
+if (temperatura >= 15 && temperatura <= 28) 
+{
+    Console.WriteLine("el clima esta templado");
+}
+if (temperatura >= 28)
+{
+    Console.WriteLine("esta haciendo calor");
+}
