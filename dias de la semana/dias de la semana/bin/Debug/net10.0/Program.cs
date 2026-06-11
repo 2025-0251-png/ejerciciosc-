11145b99d146c8c08cb77569122a9196f914101d
2025-0251-png <2025-0251@unad.edu.do>
2026-06-10 23:34:49 -0400

    Add files via upload

---
+++ dias de la semana/dias de la semana/bin/Debug/net10.0/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("seleccione un numero del 1 al 7 para saber el dia ");
+int dia = int.Parse(Console.ReadLine());
+
+switch (dia)
+{
+    case 1:
+        Console.WriteLine("Lunes");
+        break;
+    case 2:
+        Console.WriteLine("Martes");
+        break;
+    case 3:
+        Console.WriteLine("Miercoles");
+        break;
+    case 4:
+        Console.WriteLine("Jueves");
+        break;
+    case 5:
+        Console.WriteLine("Viernes");
+        break;
+    case 6:
+        Console.WriteLine("Sabado");
+        break;
+    case 7:
+        Console.WriteLine("Domingo");
+        break;
+    default:
+        Console.WriteLine("Numero no valido, por favor ingrese un numero del 1 al 7.");
+        break;
+}
